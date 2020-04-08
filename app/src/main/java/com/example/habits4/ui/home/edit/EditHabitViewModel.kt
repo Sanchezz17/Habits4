51379d6b8bcb67a743fc0504bff1153d7e8d2df2
Sanchezz17 <gekachan2000@gmail.com>
2020-04-08 12:15:45 +0500

    7 задача

--- app/src/main/java/com/example/habits4/ui/home/edit/EditHabitViewModel.kt
+++ app/src/main/java/com/example/habits4/ui/home/edit/EditHabitViewModel.kt
@@ -4,18 +4,28 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.habits4.App
 import com.example.habits4.database.Habit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class EditHabitViewModel(private val habitId: Int) : ViewModel() {
     val habit: LiveData<Habit?> = App.database.habitDao().getById(habitId)
 
-    fun saveHabit(habit: Habit) {
+    fun postSaveHabit(habit: Habit) {
+        GlobalScope.launch(Dispatchers.Default) {
+            saveHabit(habit)
+        }
+    }
+
+    private suspend fun saveHabit(habit: Habit) {
         val habitDao = App.database.habitDao()
         if (habitId == Habit.INVALID_ID) {
-            habitDao.insert(habit)
+            withContext(Dispatchers.IO) { habitDao.insert(habit) }
         } else {
             habit.id = habitId
-            habitDao.update(habit)
+            withContext(Dispatchers.IO) { habitDao.update(habit) }
         }
     }
 }
